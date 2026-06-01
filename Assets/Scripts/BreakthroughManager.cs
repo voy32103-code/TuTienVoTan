fb6b003e32fbc7d8567605c7bf6ca650a44e3cbf
Hung Yen <voy32103@gmail.com>
2026-06-02 03:11:04 +0700

    Add breakthrough event UI

--- Assets/Scripts/BreakthroughManager.cs
+++ Assets/Scripts/BreakthroughManager.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class BreakthroughManager : MonoBehaviour
 {
     public PlayerCultivation playerCultivation;
     public EnemySpawner enemySpawner;
+
+    public GameObject breakthroughPanel;
+    public TMP_Text breakthroughText;
 
     public float breakthroughDuration = 30f;
 
@@ -13,6 +17,11 @@ public class BreakthroughManager : MonoBehaviour
     void Start()
     {
         playerCultivation.OnBreakthroughRequired += StartBreakthroughEvent;
+
+        if (breakthroughPanel != null)
+        {
+            breakthroughPanel.SetActive(false);
+        }
     }
 
     void Update()
@@ -23,6 +32,18 @@ public class BreakthroughManager : MonoBehaviour
         }
 
         timer += Time.deltaTime;
+
+        float remainingTime = breakthroughDuration - timer;
+
+        if (breakthroughText != null)
+        {
+            breakthroughText.text =
+                "TIẾN GIAI BẮT ĐẦU\n"
+                + "Quái triều đang tập kích!\n"
+                + "Sống sót: "
+                + Mathf.CeilToInt(remainingTime)
+                + "s";
+        }
 
         if (timer >= breakthroughDuration)
         {
@@ -37,6 +58,11 @@ public class BreakthroughManager : MonoBehaviour
 
         enemySpawner.StartTribulationMode();
 
+        if (breakthroughPanel != null)
+        {
+            breakthroughPanel.SetActive(true);
+        }
+
         Debug.Log("===== TIẾN GIAI BẮT ĐẦU =====");
         Debug.Log("Sống sót trong " + breakthroughDuration + " giây để đột phá cảnh giới!");
     }
@@ -46,6 +72,11 @@ public class BreakthroughManager : MonoBehaviour
         isBreakthroughActive = false;
 
         enemySpawner.StopTribulationMode();
+
+        if (breakthroughPanel != null)
+        {
+            breakthroughPanel.SetActive(false);
+        }
 
         playerCultivation.CompleteBreakthrough();
 
