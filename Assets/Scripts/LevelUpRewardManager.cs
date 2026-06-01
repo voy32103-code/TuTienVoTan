5682c113881a08358ff22c25bd9fae82523d37d5
Hung Yen <voy32103@gmail.com>
2026-06-02 02:47:46 +0700

    Add level up reward popup UI

--- Assets/Scripts/LevelUpRewardManager.cs
+++ Assets/Scripts/LevelUpRewardManager.cs
@@ -1,66 +1,65 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelUpRewardManager : MonoBehaviour
 {
     public PlayerCultivation playerCultivation;
     public TechniqueManager techniqueManager;
-    public PlayerAutoAttack playerAutoAttack;
-    public PlayerMovement playerMovement;
+
+    public GameObject rewardPanel;
+
+    public Button rewardButton1;
+    public Button rewardButton2;
+    public Button rewardButton3;
+
+    public TMP_Text rewardButton1Text;
+    public TMP_Text rewardButton2Text;
+    public TMP_Text rewardButton3Text;
 
     void Start()
     {
         playerCultivation.OnLevelUp += ShowRewardChoices;
-    }
 
-    void Update()
-    {
-        if (Time.timeScale == 0f && Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            ChooseReward1();
-        }
+        rewardPanel.SetActive(false);
 
-        if (Time.timeScale == 0f && Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            ChooseReward2();
-        }
-
-        if (Time.timeScale == 0f && Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            ChooseReward3();
-        }
+        rewardButton1.onClick.AddListener(ChooseReward1);
+        rewardButton2.onClick.AddListener(ChooseReward2);
+        rewardButton3.onClick.AddListener(ChooseReward3);
     }
 
     void ShowRewardChoices()
     {
         Time.timeScale = 0f;
 
-        Debug.Log("LEVEL UP REWARD!");
-        Debug.Log("1. Nâng Dẫn Khí Quyết");
-        Debug.Log("2. Nâng Ngự Kiếm Thuật");
-        Debug.Log("3. Nâng Dẫn Lôi Thuật");
+        rewardPanel.SetActive(true);
 
+        rewardButton1Text.text = "Dẫn Khí Quyết\nTăng hấp thu linh khí";
+        rewardButton2Text.text = "Ngự Kiếm Thuật\nTăng sát thương phi kiếm";
+        rewardButton3Text.text = "Dẫn Lôi Thuật\nTăng sức mạnh thiên lôi";
     }
 
     void ChooseReward1()
     {
         techniqueManager.UpgradeTechnique("Dẫn Khí Quyết");
-
-        Time.timeScale = 1f;
-        Debug.Log("Đã chọn: Nâng Dẫn Khí Quyết");
+        CloseRewardPanel();
     }
+
     void ChooseReward2()
     {
         techniqueManager.UpgradeTechnique("Ngự Kiếm Thuật");
-
-        Time.timeScale = 1f;
-        Debug.Log("Đã chọn: Nâng Ngự Kiếm Thuật");
+        CloseRewardPanel();
     }
 
     void ChooseReward3()
     {
         techniqueManager.UpgradeTechnique("Dẫn Lôi Thuật");
+        CloseRewardPanel();
+    }
 
+    void CloseRewardPanel()
+    {
+        rewardPanel.SetActive(false);
         Time.timeScale = 1f;
-        Debug.Log("Đã chọn: Nâng Dẫn Lôi Thuật");
     }
 }
